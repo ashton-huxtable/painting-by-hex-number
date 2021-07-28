013ccac9b9fba0ca44454363c8990e47928e6da7
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-28 08:33:09 -0600

    Export SwatchPicker to Home

--- src/components/ColorPalette/ColorPalette.tsx
+++ src/components/ColorPalette/ColorPalette.tsx
@@ -1,1 +1,15 @@
-export {}
+import React from 'react';
+import { SwatchesPicker } from 'react-color';
+import { useState } from 'react';
+
+
+const ColorPalette: React.FC = () => {
+
+  return(
+    <div>
+      <SwatchesPicker />
+    </div>
+  )
+}
+
+export default ColorPalette

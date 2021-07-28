97eccdcfe195ded18c981590e70686b815e2cbfb
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-28 08:34:56 -0600

    Add interface and hook to set state for chosen color

--- src/components/ColorPalette/ColorPalette.tsx
+++ src/components/ColorPalette/ColorPalette.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { SwatchesPicker } from 'react-color';
 import { useState } from 'react';
 
+export interface ColorResult {
+  hex: string;
+}
 
 const ColorPalette: React.FC = () => {
 
+  const [chosenColor, setChosenColor] = useState({})
+
+  const handleChange = (color: ColorResult, event: React.ChangeEvent<HTMLInputElement>) => {
+      setChosenColor(color.hex)
+  }
+
   return(
     <div>
-      <SwatchesPicker />
+      <SwatchesPicker onChange={handleChange}/>
     </div>
   )
 }

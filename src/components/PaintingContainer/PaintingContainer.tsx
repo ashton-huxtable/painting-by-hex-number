9ca328731afd6d0a2d16b2549823db894b8179c2
lourdesmendoza <lourd.mendoza@gmail.com>
2021-07-27 21:07:58 -0500

    Break down component for single image container

--- src/components/PaintingContainer/PaintingContainer.tsx
+++ src/components/PaintingContainer/PaintingContainer.tsx
@@ -1,1 +1,31 @@
-export {}
+
+import React from 'react'
+import { useState, useEffect } from 'react'
+
+// export interface ArtProps {}
+
+export interface ArtsyProps {
+    id: string
+    title: string
+    artist: string
+    longTitle: string
+    webImage: {
+        url: string
+    }
+    
+}
+
+const PaintingContainer: React.FC<ArtsyProps> = ({id, title, artist, longTitle, webImage}) => {
+
+      return (
+          
+      <div className='match-container'>
+          <h2>{longTitle}</h2>
+          <h3>{artist}</h3>
+          <img src={webImage.url} className={id} alt={title}></img>
+          {/* longTitle={piece.longTitle}     */}
+      </div>
+      )
+  }
+
+export default PaintingContainer;

2c806da4efe6a402838d0c43240e085d7a85ceff
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-31 13:51:10 -0600

    Build out Favorites and add interface with props

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,1 +1,31 @@
-export {}
+import React from 'react';
+import FavoriteContainer from '../FavoriteContainer/FavoriteContainer'
+
+export interface AllFavoritesProps{
+  favorites: Favorite[]
+}
+
+export interface Favorite {
+  id: string
+  title: string
+  longTitle: string
+  principalOrFirstMaker: string
+  webImage: {
+      url: string
+  }
+}
+
+const Favorites: React.FC<AllFavoritesProps>= (props) => {
+
+
+
+  return(
+    <section className='favorites-container'>
+      {eachFavorite}
+    </section>
+      
+  )
+
+}
+
+export default Favorites;

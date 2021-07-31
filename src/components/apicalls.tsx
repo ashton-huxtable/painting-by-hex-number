c8131d65df4cdf0ca76cb497409436b9cbba7628
Em Orendorff <em.orendorff@gmail.com>
2021-07-31 11:58:59 -0500

    Change color scheme to account for vis. Add subheading description

--- src/components/apicalls.tsx
+++
@@ -1,7 +0,0 @@
-export const getAllArt = (hexId: string) => {
-    return fetch(`https://www.rijksmuseum.nl/api/en/collection?key=SkU9wRGq&f.normalized32Colors.hex=%20%23${hexId}`)
-    .then(response => response.json())
-    .catch(error => error)
-}
-
-

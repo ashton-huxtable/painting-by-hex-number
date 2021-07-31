91e479bf0cd604acf9308f23f277b04346346efc
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-31 13:52:24 -0600

    Add interface and build our favoriteContainer

--- src/components/FavoriteContainer/FavoriteContainer.tsx
+++ src/components/FavoriteContainer/FavoriteContainer.tsx
@@ -1,8 +1,29 @@
 import React from "react";
 
-const FavoriteContainer = () => {
+export interface EachFavoriteProps {
+  id: string
+  title: string
+  longTitle: string
+  principalOrFirstMaker: string
+  webImage: {
+      url: string
+  }
+  year: string
+}
+
+const FavoriteContainer: React.FC<EachFavoriteProps> = 
+  ({id, title, longTitle, principalOrFirstMaker, webImage, year}) => {
   return (
-    <h1>favorite painting</h1>
+   
+    <article className='each-favorite'>
+        <img className='image' src={webImage.url} alt={longTitle}></img>
+            <div className='middle'>
+                <div className='text'>
+                    <h3>{title}</h3>
+                    <p>{principalOrFirstMaker}, {year}</p>
+                </div>
+            </div>
+      </article>
   )
 
 }
